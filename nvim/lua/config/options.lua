e0156ccb28dc9c5b4e24ddb0ddbf29535b046547
Thomas Lee <thomaslwk1@gmail.com>
2025-02-05 15:07:46 +0800

    nvim: add quickscope

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- options
 
 vim.g.lazyvim_php_lsp = "intelephense"
+
+-- Trigger a highlight in the aepropriate direction when pressing these keys:
+-- vim.cmd('let g:qs_highlight_on_keys = [\'f\', \'F\', \'t\', \'T\']')
+vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
+vim.g.qs_max_chars = 150
