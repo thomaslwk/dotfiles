85a1ef983622867501f8dc1a4d4993c9f5471e76
Thomas Lee <thomaslwk1@gmail.com>
2025-02-05 15:11:45 +0800

    nvim: update indent shortcut

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,8 @@
 -- Keymaps
--- Set space as leader
--- vim.g.mapleader = " "
--- vim.g.maplocalleader = " "
 
 -- Set jk for ESC
 vim.keymap.set("i", "jk", "<Esc>")
+
+-- Indentation
+vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, silent = true })
