bf03c06a7168492acbdeff2cbc9936bc6576746d
Thomas Lee <thomaslwk1@gmail.com>
2025-03-02 20:10:56 +0800

    wip

--- .linux/workspace.sh
+++ .linux/workspace.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# Define the number of workspaces (Adjust if needed)
 NUM_WORKSPACES=6
 
-# Workspace navigation (Alt + 1-9)
+# Workspace navigation 
 for i in $(seq 1 $NUM_WORKSPACES); do
   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$((i)) "['<Alt>$i']"
 done
 
-# Move active window to workspace (Alt + Shift + 1-9)
+# Move active window to workspace 
 for i in $(seq 1 $NUM_WORKSPACES); do
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$((i)) "['<Alt><Shift>$i']"
 done
 
-# Window management shortcuts (similar to i3)
+# Window management shortcuts 
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>q']"            # Close window
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Alt>f']"         # Toggle fullscreen
 gsettings set org.gnome.desktop.wm.keybindings minimize "['<Alt>m']"         # Minimize window
 gsettings set org.gnome.desktop.wm.keybindings toggle-maximized "['<Alt>t']" # Toggle maximized state
+
+# Move to workspace left and right
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Alt>a']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Alt>s']"
 
 # Application Launcher (Alt + Space for rofi/dmenu)
 gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "['<Alt>space']"
