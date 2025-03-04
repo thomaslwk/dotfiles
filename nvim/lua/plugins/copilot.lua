e125005e1e5d8a1b1aae95e18c98a394e2800605
Thomas Lee <thomaslwk1@gmail.com>
2025-03-04 23:21:51 +0800

    update nvim

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+-- Copilot
+
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = { enabled = false },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-h>",
+        },
+      },
+    })
+  end,
+}
